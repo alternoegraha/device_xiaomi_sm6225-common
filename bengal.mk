c22cbbf9f37e0b44e365e0c877cdf9a1a29dbc8e
boedhack99 <boedhack@gmail.com>
2023-01-28 16:08:47 +0700

    sm6225-common: usb: Update USB shell scripts from LA.UM.9.15.r1-07100-KAMORTA.QSSI13.0
    Change-Id: I701a03d3d16975f0373720397d0ef734fd65ddee

--- bengal.mk
+++ bengal.mk
@@ -333,14 +333,12 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
     init.qti.dcvs.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom_ramdisk \
     init.qcom.rc \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
@@ -410,6 +408,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-xiaomi_bengal \
     android.hardware.usb.gadget@1.0-service-xiaomi_bengal
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.debug.rc \
+    init.qcom.usb.debug.sh\
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
